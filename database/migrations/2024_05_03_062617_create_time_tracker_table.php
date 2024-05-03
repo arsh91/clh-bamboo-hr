28bccfc5d523242ddbf3a1a246e99aac7e42586a
simran-code4each <>
2024-05-03 12:03:06 +0530

    add table

---
+++ database/migrations/2024_05_03_062617_create_time_tracker_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('time_tracker', function (Blueprint $table) {
+            $table->id();
+            $table->string('emp_table_id')->nullable();
+            $table->integer('emp_id')->nullable();
+            $table->integer('type')->nullable();
+            $table->date('expiration_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('time_tracker');
+    }
+};
